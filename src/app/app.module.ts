053426164936fef721e69e45273e29f139376eff
Benjamin Navarro <benjamin.navarro.m@outlook.cl>
2021-10-27 20:48:37 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { NewComponent } from './new/new.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    NewComponent,
+    AppFooterComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
